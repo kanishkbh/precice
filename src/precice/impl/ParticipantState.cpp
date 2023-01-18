b93da8f7631d5043c5f654be2c9cc0322c3925c6
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-01-18 19:40:50 +0100

    Add function to append global data contexts in Participant.

--- src/precice/impl/ParticipantState.cpp
+++ src/precice/impl/ParticipantState.cpp
@@ -121,6 +121,15 @@ void ParticipantState::addReadData(
   _readDataContexts.emplace(MeshDataKey{mesh->getName(), data->getName()}, ReadDataContext(data, mesh, interpolationOrder));
 }
 
+void Participant::addGlobalData(
+    const mesh::PtrData &data,
+    int                  interpolationOrder)
+{
+  // checkDuplicatedData(data, mesh->getName());
+  //TODO: add support for global data in checkDuplicatedData
+  _globalDataContexts.emplace(data->getID(), GlobalDataContext(data, interpolationOrder));
+}
+
 void ParticipantState::addReadMappingContext(
     const MappingContext &mappingContext)
 {
