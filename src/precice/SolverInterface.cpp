b04dbd20fbd143351b9b0621f48e88f81daa0ad0
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-01-27 01:50:47 +0100

    Fix compile errors.

--- src/precice/SolverInterface.cpp
+++ src/precice/SolverInterface.cpp
@@ -103,7 +103,7 @@ bool SolverInterface::hasGlobalData(
 int SolverInterface::getGlobalDataID(
     const std::string &dataName) const
 {
-  return _impl->getGlobalDataID(dataName, meshID);
+  return _impl->getGlobalDataID(dataName);
 }
 
 // void SolverInterface:: resetMesh
