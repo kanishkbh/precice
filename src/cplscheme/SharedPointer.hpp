d7669a9305682763467168c8980190c6a7391f0d
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-02-15 17:52:22 +0100

    Add GlobalCouplingData.

--- src/cplscheme/SharedPointer.hpp
+++ src/cplscheme/SharedPointer.hpp
@@ -8,10 +8,12 @@ namespace cplscheme {
 class CouplingScheme;
 class CouplingSchemeConfiguration;
 class CouplingData;
+class GlobalCouplingData;
 
 using PtrCouplingScheme              = std::shared_ptr<CouplingScheme>;
 using PtrCouplingSchemeConfiguration = std::shared_ptr<CouplingSchemeConfiguration>;
 using PtrCouplingData                = std::shared_ptr<CouplingData>;
+using PtrGlobalCouplingData          = std::shared_ptr<GlobalCouplingData>;
 
 } // namespace cplscheme
 } // namespace precice
