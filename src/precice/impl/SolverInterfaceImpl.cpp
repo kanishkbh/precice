204e8a226e5a1aade0840bc9c2af799b99a2fd8f
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-03-07 15:55:28 +0100

    Fix more compile errors introduced during rebase.

--- src/precice/impl/SolverInterfaceImpl.cpp
+++ src/precice/impl/SolverInterfaceImpl.cpp
@@ -1586,7 +1586,6 @@ void SolverInterfaceImpl::mapReadData()
     }
     context.storeDataInWaveform();
   }
-  clearMappings(_accessor->readMappingContexts());
 
   for (auto &context : _accessor->globalDataContexts()) {
     if (context.getDirection() == "read") {
