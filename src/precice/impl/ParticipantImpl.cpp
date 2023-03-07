fa86b547ceffca9f27a5cd7712e23e72a2bc0714
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-03-07 15:55:28 +0100

    Fix more compile errors introduced during rebase.

--- src/precice/impl/ParticipantImpl.cpp
+++ src/precice/impl/ParticipantImpl.cpp
@@ -1583,7 +1583,6 @@ void ParticipantImpl::mapReadData()
       context.mapData();
     }
   }
-  clearMappings(_accessor->readMappingContexts());
 
   for (auto &context : _accessor->globalDataContexts()) {
     if (context.getDirection() == "read") {
