0cd0619b0e05e23b7266f1a038380a08acfa71c9
Kanishk Bhatia <104208031+kanishkbh@users.noreply.github.com>
2023-04-07 21:38:12 +0200

    Remove commented out code.
    Co-authored-by: Benjamin Uekermann <benjamin.uekermann@gmail.com>

--- src/precice/impl/ParticipantImpl.cpp
+++ src/precice/impl/ParticipantImpl.cpp
@@ -1120,7 +1120,6 @@ void ParticipantImpl::writeGradientData(
 
 void SolverInterfaceImpl::writeGlobalVectorData(
     int dataID,
-    // int           valueIndex,
     const double *value)
 {
   PRECICE_TRACE(dataID);
