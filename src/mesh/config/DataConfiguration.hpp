dfc2d902369fda39c9ca3dd3c298f68d460e3342
Kanishk Bhatia <kanishk.bhatia@tum.de>
2023-05-01 19:01:43 +0200

    Replace GlobalData with Data

--- src/mesh/config/DataConfiguration.hpp
+++ src/mesh/config/DataConfiguration.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include "logging/Logger.hpp"
 #include "mesh/Data.hpp"
-#include "mesh/GlobalData.hpp"
+// #include "mesh/GlobalData.hpp"
 #include "mesh/SharedPointer.hpp"
 #include "utils/ManageUniqueIDs.hpp"
 #include "xml/XMLTag.hpp"
@@ -34,7 +34,7 @@ public:
   const std::vector<ConfiguredData> &data() const;
 
   /// Returns the Global Data with the matching name
-  const PtrGlobalData &globalData(const std::string &dataName) const;
+  const PtrData &globalData(const std::string &dataName) const;
 
   /// Returns whether Data Configuration has Global Data with the dataName
   bool hasGlobalDataName(const std::string &dataName) const;
@@ -73,7 +73,7 @@ private:
 
   std::vector<ConfiguredData> _data;
 
-  std::vector<PtrGlobalData> _globalData;
+  std::vector<PtrData> _globalData;
 
   int _indexLastConfigured = -1;
 
